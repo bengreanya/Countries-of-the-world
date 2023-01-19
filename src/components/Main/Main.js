9f9f0bffbaaf271b10bf4c639a57a9abb38c942c
Ben Greanya <bengreanya@gmail.com>
2023-01-19 08:53:39 -0800

    continents displaying in dropdown but no countries rendering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,29 +5,31 @@ import { useState } from 'react';
 
 export default function Main() {
   const { countries, error } = useCountries();
-  const { continent, setContinent } = useState('all');
-  const continents = [...new Set(countries.map(({ continent }) => continent))];
-  const filtered = countries.filter(
-    (country) => country.continent === continent || continent === 'all'
-  );
+  //   const { continent, setContinent } = useState('all');
+  //   const continents = [...new Set(countries.map(({ continent }) => continent))];
+  //   const filtered = countries.filter(
+  //     (country) => country.continent === continent || continent === 'all'
+  //   );
   return (
-    // <main>
-    //   <p>{error}</p>
-    //   {countries.map((country) => (
-    //     <CountryCard key={country.id} {...country} />
-    //   ))}
-    // </main>
     <main>
-      <select onChange={(e) => setContinent(e.target.value)}>
-        <option value="all">all</option>
-        {continents.map((continent) => (
-          <option key={continent} value={continent}></option>
-        ))}
-      </select>
       <p>{error}</p>
-      {filtered.map((country) => (
+      {countries.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
     </main>
+    // <main>
+    //   <select onChange={(e) => setContinent(e.target.value)}>
+    //     <option value="all">all</option>
+    //     {continents.map((continent) => (
+    //       <option key={continent} value={continent}>
+    //         {continent}
+    //       </option>
+    //     ))}
+    //   </select>
+    //   <p>{error}</p>
+    //   {filtered.map((country) => (
+    //     <CountryCard key={country.id} {...country} />
+    //   ))}
+    // </main>
   );
 }
