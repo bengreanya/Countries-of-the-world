6686dd6c45c722a09ec2fd3c8f20abb7f2c596aa
Ben Greanya <bengreanya@gmail.com>
2023-01-18 16:32:46 -0800

    added partial option code

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useCountries } from '../../hooks/useCountries';
 import CountryCard from '../CountryCard/CountryCard.js';
+import { useState } from 'react';
 
 export default function Main() {
   const { countries, error } = useCountries();
+  //   const { continent, setContinent } = useState('all');
+  //   const continents = [...new Set(countries.map(({ continent }) => continent))];
+  //   const filtered = countries.filter((countries) => );
   return (
     <main>
       <p>{error}</p>
@@ -11,5 +15,19 @@ export default function Main() {
         <CountryCard key={country.id} {...country} />
       ))}
     </main>
+    // <main>
+    //   <select onChange={(e) => setContinent(e.target.value)}>
+    //     <option value="all">all</option>
+    //     {continent.map((type) => (
+    //       <option key={continent} value={continent}>
+    //         {continent}
+    //       </option>
+    //     ))}
+    //   </select>
+    //   <p>{error}</p>
+    //   {filtered.map((continent) => (
+    //     <CountryCard key={countries.name} {...countries} />
+    //   ))}
+    // </main>
   );
 }
