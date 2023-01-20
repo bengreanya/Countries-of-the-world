94dd4c045c676d2ae81c0fe922eb22882c0dd78d
Ben Greanya <bengreanya@gmail.com>
2023-01-19 16:41:38 -0800

    everything is working now

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { useCountries } from '../../hooks/useCountries';
 import CountryCard from '../CountryCard/CountryCard.js';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export default function Main() {
   const { countries, error } = useCountries();
-  //   const { continent, setContinent } = useState('all');
-  //   const continents = [...new Set(countries.map(({ continent }) => continent))];
-  //   const filtered = countries.filter(
-  //     (country) => country.continent === continent || continent === 'all'
-  //   );
+  const [continent, setContinent] = useState('all');
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
   return (
-    <main>
-      <p>{error}</p>
-      {countries.map((country) => (
-        <CountryCard key={country.id} {...country} />
-      ))}
-    </main>
     // <main>
-    //   <select onChange={(e) => setContinent(e.target.value)}>
-    //     <option value="all">all</option>
-    //     {continents.map((continent) => (
-    //       <option key={continent} value={continent}>
-    //         {continent}
-    //       </option>
-    //     ))}
-    //   </select>
     //   <p>{error}</p>
-    //   {filtered.map((country) => (
+    //   {countries.map((country) => (
     //     <CountryCard key={country.id} {...country} />
     //   ))}
     // </main>
+    <main>
+      <select onChange={(e) => setContinent(e.target.value)}>
+        <option value="all">all</option>
+        {continents.map((continent) => (
+          <option key={continent} value={continent}>
+            {continent}
+          </option>
+        ))}
+      </select>
+      <p>{error}</p>
+      {filtered.map((country) => {
+        return <CountryCard key={country.id} {...country} />;
+      })}
+    </main>
   );
 }
